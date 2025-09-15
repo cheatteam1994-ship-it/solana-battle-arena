1f65cbda457834fd5b90942fc999e5b9c41a6cf3
cheatteam1994-ship-it <cheat.team1994@gmail.com>
2025-09-15 18:02:13 +0200

    Delete solana-battle-arena/holders.js

--- solana-battle-arena/holders.js
+++
@@ -1,8 +0,0 @@
-// Esempio di holders di un token Solana
-const holders = [
-    { wallet: "Wallet1" },
-    { wallet: "Wallet2" },
-    { wallet: "Wallet3" },
-    { wallet: "Wallet4" },
-    { wallet: "Wallet5" }
-];
